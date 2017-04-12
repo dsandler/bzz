e422133cb2163bc7e21e66cfb0fa831fbcf4a61f
Dan Sandler <dsandler@android.com>
2017-04-12 13:18:36 -0400

    Update to current Swift language.
    Also add an optional third parameter for
    NSHapticFeedbackPattern (0, 1, or 2).

--- bzz/main.swift
+++ bzz/main.swift
@@ -1,12 +1,12 @@
 import Foundation
 import AppKit
 
-func arg(i: Int, defaultValue: UInt32) -> UInt32 {
-    if Process.arguments.count > i {
-        if let val = UInt32(Process.arguments[i]) {
+func arg(_ i: Int, defaultValue: UInt32) -> UInt32 {
+    if CommandLine.arguments.count > i {
+        if let val = UInt32(CommandLine.arguments[i]) {
             return val
         } else {
-            print("not int: \(Process.arguments[i])")
+            print("not int: \(CommandLine.arguments[i])")
             exit(1)
         }
     }
@@ -14,9 +14,14 @@ func arg(i: Int, defaultValue: UInt32) -> UInt32 {
 }
 
 var length = arg(1, defaultValue: 1000)
-var interval = arg(2, defaultValue: 10)
+var interval = arg(2, defaultValue: 20)
+var pattern = NSHapticFeedbackPattern(rawValue: Int(arg(3, defaultValue: 0)))!
 
-if Process.arguments.count > 3 {
+if CommandLine.arguments.count == 1 {
+    print("usage: " + CommandLine.arguments[0] + " [length [interval [patterncode]]]");
+    exit(1)
+}
+if CommandLine.arguments.count > 4 {
     print("too many arguments")
     exit(1)
 }
@@ -27,6 +32,6 @@ if interval > length {
 
 var performer = NSHapticFeedbackManager.defaultPerformer()
 for _ in 1...(length/interval) {
-    performer.performFeedbackPattern(NSHapticFeedbackPattern.Generic, performanceTime: NSHapticFeedbackPerformanceTime.Now)
+    performer.perform(pattern, performanceTime: NSHapticFeedbackPerformanceTime.now)
     usleep(interval*1000)
 }
